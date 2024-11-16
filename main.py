fed71c0fbff8758859a2cb7c68a19c87636d7ec4
valmunos <valmunos@gmail.com>
2024-11-16 22:51:07 +0100

    add player to main loop and containers for drawing and updating

--- main.py
+++ main.py
@@ -3,23 +3,39 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
     print('Starting asteroids!')
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    # start writing code
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
     clock = pygame.time.Clock()
     dt = 0
+
+    # main game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
         screen.fill('black')
-        pygame.display.flip()
 
+        for item in updatable:
+            item.update(dt)
+        
+        for item in drawable:
+            item.draw(screen)
+
+        pygame.display.flip()
+        # pygame.display.update()
         dt = clock.tick(60) / 1000
 
 
