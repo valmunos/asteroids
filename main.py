4800d3280b51cb998a4869b6cfc3e1de04383c22
valmunos <valmunos@gmail.com>
2024-11-16 18:05:55 +0100

    add pygame clock and tick to main

--- main.py
+++ main.py
@@ -11,12 +11,16 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill('black')
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == '__main__':
